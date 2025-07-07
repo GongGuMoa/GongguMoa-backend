0085838153702677d571334358817da47e03696a
jiyun921 <ryn0921@naver.com>
2025-07-07 19:53:12 +0900

    [feat]: UserController signup 구현

--- src/main/java/com/gonggumoa/gonggumoa/domain/user/controller/UserController.java
+++ src/main/java/com/gonggumoa/gonggumoa/domain/user/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.gonggumoa.gonggumoa.domain.user.controller;
 
+import com.gonggumoa.gonggumoa.domain.user.dto.request.PostUserSignUpRequest;
+import com.gonggumoa.gonggumoa.domain.user.dto.response.PostUserSignUpResponse;
+import com.gonggumoa.gonggumoa.domain.user.service.UserService;
+import com.gonggumoa.gonggumoa.global.response.BaseResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public BaseResponse<PostUserSignUpResponse> login(@Valid @RequestBody PostUserSignUpRequest request) {
+        return BaseResponse.ok(userService.signUp(request));
+    }
+
 }
