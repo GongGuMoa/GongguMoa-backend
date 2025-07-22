b38c4fffef579a3adf14ba0386bce5d19365c5af
jiyun921 <ryn0921@naver.com>
2025-07-23 03:49:00 +0900

    [feat]: PostUserCheckEmailCodeRequest 구현

--- src/main/java/com/gonggumoa/domain/user/dto/request/PostUserCheckEmailCodeRequest.java
+++ src/main/java/com/gonggumoa/domain/user/dto/request/PostUserCheckEmailCodeRequest.java
@@ -4,9 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record PostUserEmailCodeRequest(
+public record PostUserCheckEmailCodeRequest(
         @Schema(description = "이메일", example = "test@email.com")
         @NotBlank
         @Email
-        String email
-) { }
+        String email,
+        @Schema(description = "인증코드", example = "288977")
+        @NotBlank
+        String code
+) {
+}
---
+++ src/main/java/com/gonggumoa/domain/user/dto/request/PostUserSendEmailCodeRequest.java
@@ -0,0 +1,12 @@
+package com.gonggumoa.domain.user.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record PostUserSendEmailCodeRequest(
+        @Schema(description = "이메일", example = "test@email.com")
+        @NotBlank
+        @Email
+        String email
+) { }
