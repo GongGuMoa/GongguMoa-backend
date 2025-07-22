0cac74593a5db2be4980f4647985ecf56a92498f
jiyun921 <ryn0921@naver.com>
2025-07-23 03:10:12 +0900

    [feat]: UserService - validateNickname,sendEmailVerificationCode 구현

--- src/main/java/com/gonggumoa/domain/user/service/UserService.java
+++ src/main/java/com/gonggumoa/domain/user/service/UserService.java
@@ -1,15 +1,21 @@
 package com.gonggumoa.domain.user.service;
 
 import com.gonggumoa.domain.user.domain.User;
+import com.gonggumoa.domain.user.dto.request.PostUserEmailCodeRequest;
 import com.gonggumoa.domain.user.dto.request.PostUserSignUpRequest;
 import com.gonggumoa.domain.user.dto.response.PostUserSignUpResponse;
 import com.gonggumoa.domain.user.exception.*;
 import com.gonggumoa.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -21,6 +27,8 @@ import static com.gonggumoa.global.response.status.BaseExceptionResponseStatus.*
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender mailSender;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public void validateEmail(String email) {
         if (userRepository.existsByEmail(email)) {
@@ -34,13 +42,15 @@ public class UserService {
         }
     }
 
+    public void validateNickname(String nickname) {
+        if (userRepository.existsByNickname(nickname)) {
+            throw new NicknameAlreadyExistsException(NICKNAME_ALREADY_EXISTS);
+        }
+    }
+
     public PostUserSignUpResponse signUp(PostUserSignUpRequest req) {
         if (!req.password().equals(req.passwordConfirm())) {
             throw new PasswordsNotMatchException(PASSWORDS_NOT_MATCH);
-        }
-
-        if (userRepository.existsByNickname(req.nickname())) {
-            throw new NicknameAlreadyExistsException(NICKNAME_ALREADY_EXISTS);
         }
 
         LocalDate birthdate;
@@ -65,4 +75,18 @@ public class UserService {
         return PostUserSignUpResponse.of(user.getUserId());
     }
 
+    public void sendEmailVerificationCode(PostUserEmailCodeRequest req) {
+        String code = String.valueOf((int)(Math.random() * 900000) + 100000);
+        redisTemplate.opsForValue().set(req.email(), code, Duration.ofMinutes(3));
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(req.email());
+        message.setSubject("공구모아 이메일 인증코드");
+        message.setText("인증코드: " + code + "\n유효시간은 3분입니다.");
+        try {
+            mailSender.send(message);
+        } catch (MailException e) {
+            throw new EmailCodeSendFailedException(EMAIL_CODE_SEND_FAILED);
+        }
+    }
 }
