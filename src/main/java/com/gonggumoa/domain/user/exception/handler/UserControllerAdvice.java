7c1486aeb048f5b94f0ac1c02bb7e2927051bb25
jiyun921 <ryn0921@naver.com>
2025-07-23 03:50:53 +0900

    [feat]: EMAIL_CODE_EXPIRED, EMAIL_CODE_NOT_MATCH 예외 처리 구현

--- src/main/java/com/gonggumoa/domain/user/exception/handler/UserControllerAdvice.java
+++ src/main/java/com/gonggumoa/domain/user/exception/handler/UserControllerAdvice.java
@@ -80,5 +80,17 @@ public class UserControllerAdvice {
         return ResponseEntity.badRequest().body(new BaseErrorResponse(EMAIL_CODE_SEND_FAILED));
     }
 
+    @ExceptionHandler(EmailCodeExpiredException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<BaseErrorResponse> handleEmailCodeExpiredException(EmailCodeExpiredException e) {
+        return ResponseEntity.badRequest().body(new BaseErrorResponse(EMAIL_CODE_EXPIRED));
+    }
+
+    @ExceptionHandler(EmailCodeNotMatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<BaseErrorResponse> handleEmailCodeNotMatchException(EmailCodeNotMatchException e) {
+        return ResponseEntity.badRequest().body(new BaseErrorResponse(EMAIL_CODE_NOT_MATCH));
+    }
+
 
 }
