837708656c79ddcdc3462cc38ed7651aabe53a88
양지윤 <155087951+jiyun921@users.noreply.github.com>
2025-08-15 16:33:12 +0900

    Update UserControllerAdvice.java

--- src/main/java/com/gonggumoa/domain/user/exception/handler/UserControllerAdvice.java
+++ src/main/java/com/gonggumoa/domain/user/exception/handler/UserControllerAdvice.java
@@ -12,9 +12,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 import static com.gonggumoa.global.response.status.BaseExceptionResponseStatus.*;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice(basePackages = "com.gonggumoa.domain.user")
 public class UserControllerAdvice {
 
