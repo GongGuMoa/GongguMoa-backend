9e9d9459382c137b953be55096fb6f6473feb808
양지윤 <155087951+jiyun921@users.noreply.github.com>
2025-08-16 17:32:21 +0900

    [refactor] GetUserLocationResponse 변수명 수정

--- src/main/java/com/gonggumoa/domain/user/dto/response/GetUserLocationResponse.java
+++ src/main/java/com/gonggumoa/domain/user/dto/response/GetUserLocationResponse.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record GetUserLocationResponse (
         @Schema(description = "주소 문자열", example = "서울특별시 중구 세종대로 110")
-        String address,
+        String location,
 
         @Schema(description = "위도", example = "37.5351")
         Double latitude,
@@ -12,7 +12,7 @@ public record GetUserLocationResponse (
         @Schema(description = "경도", example = "127.0825")
         Double longitude
 ) {
-    public static GetUserLocationResponse of(String address, Double latitude, Double longitude) {
-        return new GetUserLocationResponse(address, latitude, longitude);
+    public static GetUserLocationResponse of(String location, Double latitude, Double longitude) {
+        return new GetUserLocationResponse(location, latitude, longitude);
     }
 }
